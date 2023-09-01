db201ab9d377c9161560bc14de1d24abf69921e7
Srujan Papaiahgari <srujanpapaiahgari@gmail.com>
2023-09-01 14:20:14 +0530

    removed asterik from the cors origins

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://mail.google.com/*", "https://sme-frontend.vercel.app/*"],
+    origin: ["https://mail.google.com", "https://sme-frontend.vercel.app"],
   })
 );
 app.use(express.urlencoded({ extended: false }));
