7db4efea5ff00ba99b257b6f86e076dd1f2a7dd4
Srujan Papaiahgari <srujanpapaiahgari@gmail.com>
2023-09-01 13:07:33 +0530

    shifted emailRouter to before authRouter

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/", authRouter);
 app.use("/email", emailRouter);
+app.use("/", authRouter);
 
 const port = 8080;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
